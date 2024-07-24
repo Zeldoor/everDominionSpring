845c54894642629f726dea019cc67ce86e80fb75
Lorenzo Giustozzi <alathwork@gmail.com>
2024-07-24 14:03:17 +0200

    refresh progetto

--- src/main/java/com/generation/dominion/repository/PlayerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.generation.dominion.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.generation.dominion.model.Player;
-
-public interface PlayerRepository extends JpaRepository<Player, Integer> {
-}
--- src/main/java/com/generation/dominion/repository/TroopRepository.java
+++
@@ -1,8 +0,0 @@
-package com.generation.dominion.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.generation.dominion.model.Troop;
-
-public interface TroopRepository extends JpaRepository<Troop, Integer> {
-
-}
