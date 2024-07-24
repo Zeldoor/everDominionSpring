39d79f800bc89ee4d10a8ea903623978ea5900e8
Lorenzo Giustozzi <alathwork@gmail.com>
2024-07-24 09:33:29 +0200

    decomment di alcune classi

--- src/main/java/com/generation/dominion/service/MockService.java
+++ src/main/java/com/generation/dominion/service/MockService.java
@@ -14,62 +14,62 @@ public class MockService {
 
    
 
-    // public List<PlayerDTOwTroops> generateMockPlayers(int numberOfPlayers) {
-    //     List<PlayerDTOwTroops> players = new ArrayList<>();
+     public List<PlayerDTOwTroops> generateMockPlayers(int numberOfPlayers) {
+         List<PlayerDTOwTroops> players = new ArrayList<>();
 
-    //     for (int i = 0; i < numberOfPlayers; i++) {
-    //         PlayerDTOwTroops player = new PlayerDTOwTroops();
-    //         player.setNick("Player" + (i + 1));
-    //         player.setTroops(generateRandomTroopIds());
-    //         player.setTotalMinDamage(calculateTotalMinDamage(player.getTroops()));
-    //         player.setTotalMaxDamage(calculateTotalMaxDamage(player.getTroops()));
-    //         player.setTotalHealth(calculateTotalHealth(player.getTroops()));
-    //         players.add(player);
-    //     }
+        for (int i = 0; i < numberOfPlayers; i++) {
+             PlayerDTOwTroops player = new PlayerDTOwTroops();
+             player.setNick("Player" + (i + 1));
+             player.setTroops(generateRandomTroopIds());
+             player.setTotalMinDamage(calculateTotalMinDamage(player.getTroops()));
+             player.setTotalMaxDamage(calculateTotalMaxDamage(player.getTroops()));
+             player.setTotalHealth(calculateTotalHealth(player.getTroops()));
+             players.add(player);
+         }
 
-    //     return players;
-    // }
+         return players;
+     }
 
-    // private List<TroopDTO> generateRandomTroopIds() 
-    // {
-    //     List<TroopDTO> res = new ArrayList<>();
-    //     for (int index = 0; index < 6; index++) 
-    //     {
-    //         res.add(TroopGenerator.generateRandomTroop());
-    //     }
-    //     return res;
+     private List<TroopDTO> generateRandomTroopIds() 
+     {
+         List<TroopDTO> res = new ArrayList<>();
+         for (int index = 0; index < 6; index++) 
+         {
+             res.add(TroopGenerator.generateRandomTroop());
+         }
+         return res;
         
-    // }
+     }
 
-    // private int calculateTotalMinDamage(List<TroopDTO> troops) {
-    //     int totalDamage = 0;
-    //     for (TroopDTO troop : troops) 
-    //     {
+     private int calculateTotalMinDamage(List<TroopDTO> troops) {
+         int totalDamage = 0;
+         for (TroopDTO troop : troops) 
+         {
             
-    //         totalDamage += troop.getMinimumDamage() ; // danno minimo messo nel total damage
-    //     }
-    //     return totalDamage;
-    // }
+             totalDamage += troop.getMinimumDamage() ; // danno minimo messo nel total damage
+         }
+         return totalDamage;
+     }
     
-    // private int calculateTotalMaxDamage(List<TroopDTO> troops) {
-    //     int totalDamage = 0;
-    //     for (TroopDTO troop : troops) 
-    //     {
+     private int calculateTotalMaxDamage(List<TroopDTO> troops) {
+         int totalDamage = 0;
+         for (TroopDTO troop : troops) 
+         {
             
-    //         totalDamage += troop.getMinimumDamage() ; // danno minimo messo nel total damage
-    //     }
-    //     return totalDamage;
-    // }
+             totalDamage += troop.getMinimumDamage() ; // danno minimo messo nel total damage
+         }
+         return totalDamage;
+     }
 
-    // private int calculateTotalHealth(List<TroopDTO> troops) 
-    // {
-    //     int totalHealth = 0;
-    //     for (TroopDTO troop : troops) 
-    //     {
+     private int calculateTotalHealth(List<TroopDTO> troops) 
+     {
+         int totalHealth = 0;
+         for (TroopDTO troop : troops) 
+         {
             
-    //         totalHealth += troop.getHealth() ; 
-    //     }
-    //     return totalHealth;
-    // }
+             totalHealth += troop.getHealth() ; 
+         }
+         return totalHealth;
+     }
 
 }
