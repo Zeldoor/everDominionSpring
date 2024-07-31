a9e9794c717efa007597162c6654657fd2fdf9f3
Mattia Rosati <mattiarosati98@gmail.com>
2024-07-31 15:49:49 +0200

    Aggiornamenteo securityConfig

--- src/main/java/com/generation/dominion/model/UserEntity.java
+++ src/main/java/com/generation/dominion/model/UserEntity.java
@@ -34,5 +34,4 @@ public class UserEntity {
     @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
             inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
     private List<Role> roles = new ArrayList<>();
-
 }
