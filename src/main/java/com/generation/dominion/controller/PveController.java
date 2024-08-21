6ba8c8aabe638920b90b29f9a98050a4e7f4e96c
Lorenzo Giustozzi <alathwork@gmail.com>
2024-08-21 10:58:10 +0200

    fix erroracci di import pve

--- src/main/java/com/generation/dominion/controller/PveController.java
+++ src/main/java/com/generation/dominion/controller/PveController.java
@@ -1,13 +1,29 @@
 package com.generation.dominion.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import com.generation.dominion.repository.PvePlayerRepository;
+import com.generation.dominion.model.PvePlayer;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.ResponseEntity;
+
+import com.generation.dominion.repository.PvePlayerRepository;
+import com.generation.dominion.model.PvePlayer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/pve")
 public class PveController 
 {
 
-    @Autowired 
+    @Autowired
     private PvePlayerRepository pveRepo;
 
     @GetMapping
@@ -22,8 +38,10 @@ public class PveController
     @GetMapping("/{id}")
     public ResponseEntity<?> getPvePlayer(@PathVariable Integer id) 
     {
-        if (id)
-        PvePlayer pvePlayer = playerOtp.get();
+        if(id == null)
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("PvePlayer");
+
+        PvePlayer pvePlayer = pveRepo.findById(id).get();
         return ResponseEntity.ok(pvePlayer);
     }
 }
--- src/main/java/com/generation/dominion/repository/PvePlayerRepository.java
+++ src/main/java/com/generation/dominion/repository/PvePlayerRepository.java
@@ -1,7 +1,5 @@
 package com.generation.dominion.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.generation.dominion.model.PvePlayer;
 
