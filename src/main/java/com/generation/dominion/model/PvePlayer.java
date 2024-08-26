c9ba1c5ced66d92a0dcd6c63899328c249ea0ae2
Mattia Rosati <mattiarosati98@gmail.com>
2024-08-26 18:58:15 +0200

    a

--- src/main/java/com/generation/dominion/model/PvePlayer.java
+++ src/main/java/com/generation/dominion/model/PvePlayer.java
@@ -9,6 +9,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.generation.dominion.dto.PlayerDTOwAll;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -30,6 +31,8 @@ public class PvePlayer
     private String nick;
     private int gold;
     private String icon;
+
+    @Column(columnDefinition = "TEXT")
     private String description;
 
     @Transient
